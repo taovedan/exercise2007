65de804ead3f1f9aecd9a5a75429ca3b0e7fdd4f
Tao Ve Dan <dan@LAPTOP-3H9EQFQJ>
2023-07-20 22:08:37 +0700

    add file2

---
+++ script2.sh
@@ -0,0 +1,15 @@
+mkdir app
+cd app
+mkdir blueprints static templates
+touch app.py .gitignore
+cd blueprints
+touch __init__.py
+mkdir home_page
+cd home_page
+touch __init__.py blueprint.py
+cd app
+cd static
+touch styles.css index.js
+cd app
+cd templates
+touch base.html home.html
